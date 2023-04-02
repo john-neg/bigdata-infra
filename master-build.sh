bad4a55cd3cb85bc1fae565fa6160b4e8f067e49
john_neg <john_neg@mail.ru>
2023-04-03 00:47:15 +0300

    jupyter dockerfile update

--- master-build.sh
+++ master-build.sh
@@ -4,5 +4,7 @@ docker network create -d bridge bigdata-network
 cd hadoop/base && docker build . -t hadoop-base && cd ../..
 #Create spark base image
 cd spark/base && docker build . -t spark-base && cd ../..
+#Create jupyter base image
+cd jupyter/base && docker build . -t jupyter-base && cd ../..
 #Start all containers
 docker-compose up
